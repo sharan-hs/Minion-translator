ae66988815d818acf180466689386975c1f4960e
sharan <sharan.hosamane@gmail.com>
2023-08-02 22:58:49 +0530

    Added Api call and error handling

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var inputText = document.querySelector("#input-txt");
 var outputText = document.querySelector("#output");
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-btnTranslate.addEventListener("click", function clickEventHandler() {
-    outputText.innerText = "Translated : " + inputText.value;
 
-})
+function translationURL(text) {
+    return serverURL + "?text=" + text
+}
+
+function errorHandler(error) {
+    console.log("Error occured: " + error);
+}
+
+function clickHandler() {
+    var input = inputText.value;
+
+    fetch(translationURL(input))
+        .then(Response => Response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputText.innerText = translatedText;
+        })
+        .catch(errorHandler);
+
+};
+
+btnTranslate.addEventListener("click", clickHandler);
